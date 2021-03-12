d91ab7364036b4c0a9d11c8010249e736e6a75dd
Dennis Wong <denniswong0106@gmail.com>
2021-03-12 06:58:13 +0000

    refactored into index.js(for user) and breedFetcher.js (for function). Added mocha test, not yet implimented

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require('request');
 
-const apiPath = "https://api.thecatapi.com/v1/breeds/search?q=";
-const searchBreeds = process.argv.slice(2).join('+');
-const query = apiPath + searchBreeds;
 
-request(query, (error, response, body) => {
-  if (error !== null) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('breed not found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  const apiPath = "https://api.thecatapi.com/v1/breeds/search?q=";
+  const query = apiPath + breedName;
+
+  request(query, (error, response, body) => {
+    if (error !== null) {
+      console.log(error);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log('breed not found');
+    } else {
+      console.log(data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
